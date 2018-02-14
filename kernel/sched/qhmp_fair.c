5c0a0251267f567aeffb5a952a607fc0935af2df
Ryan Andri <ryanandri@linuxmail.org>
2018-02-15 02:58:57 +0700

    sched: extend sysinterface for ARCH_CAPACITY into QHMP
    we using QHMP as kernel scheduler instead of generic.
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- kernel/sched/qhmp_fair.c
+++ kernel/sched/qhmp_fair.c
@@ -4970,6 +4970,12 @@ void relay_gfs(unsigned int gfs)
 	Lgentle_fair_sleepers = gfs;
 }
 
+static unsigned int Larch_power = 0;
+void relay_ap(unsigned int ap)
+{
+	Larch_power = ap;
+}
+
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -8150,7 +8156,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	unsigned long capacity = SCHED_CAPACITY_SCALE;
 	struct sched_group *sdg = sd->groups;
 
-	if (sched_feat(ARCH_CAPACITY))
+	if (Larch_power)
 		capacity *= arch_scale_cpu_capacity(sd, cpu);
 	else
 		capacity *= default_scale_cpu_capacity(sd, cpu);
@@ -8159,7 +8165,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 
 	sdg->sgc->capacity_orig = capacity;
 
-	if (sched_feat(ARCH_CAPACITY))
+	if (Larch_power)
 		capacity *= arch_scale_freq_capacity(sd, cpu);
 	else
 		capacity *= default_scale_capacity(sd, cpu);
